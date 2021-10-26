f558177e5bc089c4a89e5f00b4d706c7afbd8fa6
Gabriela Rodrigues <gabrielarodrigues432@gmail.com>
2021-10-26 18:04:04 -0300

    inicial commit

--- src/models/jutsu_model.py
+++ src/models/jutsu_model.py
@@ -1,1 +1,12 @@
-# Cole seu código aqui
+class Jutsu:
+    jutsu_ranks = ("D", "C", "B", "A", "S",)
+
+
+    def __init__(self, jutsu_name: str, jutsu_type: str, jutsu_level: str, jutsu_damage: int, chakra_spend: int):
+        self.jutsu_name = jutsu_name
+        self.jutsu_type = jutsu_type
+        self.jutsu_level = jutsu_level.upper() if jutsu_level.upper() in Jutsu.jutsu_ranks else "Unranked"
+        self.jutsu_damage = jutsu_damage
+        self.chakra_spend = chakra_spend if chakra_spend > 0 else 100
+
+
--- src/models/ninja_model.py
+++ src/models/ninja_model.py
@@ -1,1 +1,34 @@
-# Cole seu código aqui
+from src.models.jutsu_model import Jutsu
+class Ninja: 
+    
+
+    def __init__(self, name: str, clan: str, village: str, ninja_level="Unranked"):
+        self.name= name
+        self.clan= clan
+        self.village= village
+        self.ninja_level= ninja_level 
+        self.jutsu_list= []
+        self.health_pool= 100
+        self.chakra_pool= 100
+        self.concious= True
+
+    def learn_jutsu(self, jutsu: Jutsu):
+        self.jutsu_list.append(jutsu)
+        return f'O ninja {self.name} {self.clan} acabou de aprender um novo jutsu: {jutsu.jutsu_name}'           
+
+    def check_health(self):
+        if self.health_pool < 0:
+            self.health_pool= 0
+            self.concious= False
+        return self.concious
+
+    def cast_jutsu(self, jutsu: Jutsu, adversary: 'Ninja'):
+        if adversary.concious is False:
+            return False
+        else: 
+            if jutsu in self.jutsu_list and self.chakra_pool > jutsu.chakra_spend:
+                 adversary.health_pool -= jutsu.jutsu_damage
+                 self.chakra_pool -= jutsu.chakra_spend
+                 return True
+            return False
+
